04519e2da80f0c641c1c3509b9a8e7853b00629a
uritrejo <uritrejo20@gmail.com>
2025-05-31 01:02:21 +0200

    setup file for analysis result

---
+++ src/main/java/it/polimi/gpplib/model/GppAnalysisResult.java
@@ -0,0 +1,11 @@
+package it.polimi.gpplib.model;
+
+public class GppAnalysisResult {
+
+    /*
+     * ??++ Can it contain the same objects loaded from the json files?
+     * - or are those missing fields? Look at the response of the first tool in the
+     * notebooks
+     */
+
+}
