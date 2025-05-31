df346e98bcf3e957000c0e01a9f2291c64516a92
uritrejo <uritrejo20@gmail.com>
2025-05-31 13:29:01 +0200

    added interface & empty implementation

---
+++ src/main/java/it/polimi/gpplib/GppNoticeAnalyzer.java
@@ -0,0 +1,15 @@
+package it.polimi.gpplib;
+
+import it.polimi.gpplib.model.Notice;
+import it.polimi.gpplib.model.GppAnalysisResult;
+import it.polimi.gpplib.model.SuggestedGppPatch;
+import java.util.List;
+
+// TODO: consider separating these methods into different interfaces
+public interface GppNoticeAnalyzer {
+    Notice loadNotice(String xmlString);
+
+    GppAnalysisResult analyzeNotice(String xmlNoticeString);
+
+    String applyPatches(List<SuggestedGppPatch> patches, Notice notice);
+}
