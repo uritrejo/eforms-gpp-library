47fe635e1081dd4b7de2da0af0bdfac4ca4d3c0d
uritrejo <uritrejo20@gmail.com>
2025-05-31 12:49:51 +0200

    add notice

---
+++ src/main/java/it/polimi/gpplib/model/Notice.java
@@ -0,0 +1,12 @@
+package it.polimi.gpplib.model;
+
+public class Notice {
+    /**
+     * Represents an eform notice.
+     * It should contain the xml document tree thingie.
+     * Then it should have getters/setters for our most common fields.
+     * It should have give a toXmlString() method.
+     * Not sure if the patch application should fall directly on this mf. We'll see.
+     */
+
+}
